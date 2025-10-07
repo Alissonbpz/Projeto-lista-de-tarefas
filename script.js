381c7d630bad8e747122b9b77787c7e71e150016
Alissonbpz <Alissonbpz@gmail.com>
2025-10-06 23:29:10 -0300

    Adicionando a verificação para incluir tarefas

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+let listElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
+
+let tarefas = [];
+
+function adicionarTarefas(){
+    
+    if(inputElement.value === ""){  
+        
+        alert("Digite alguma tarefa");
+
+        return false;
+
+    } 
+    
+    else
+    {
+       let novaTarefa = inputElement.value;
+
+       tarefas.push(novaTarefa)
+       inputElement.value = " ";
+        
+    }
+    console.log(inputElement.value);
+}
+
+buttonElement.onclick = adicionarTarefas;
